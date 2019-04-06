b45d4c7d59bfe434a9e7dd0e95aece5250a6b0da
Wanvisa Suwannin <wanvisa.nun@gmail.com>
2019-04-06 16:22:11 +0700

    add plugin

---
+++ src/pages/getvalue/getvalue.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GetvaluePage } from './getvalue';
+
+@NgModule({
+  declarations: [
+    GetvaluePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GetvaluePage),
+  ],
+})
+export class GetvaluePageModule {}
